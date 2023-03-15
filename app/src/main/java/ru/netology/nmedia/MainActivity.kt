e0bd14639d45e1be8f08734d3f7c70df6e7450fe
Сергей Гришин <2709956@mail.ru>
2023-03-16 00:32:47 +0300

    icon

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -7,5 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        R.id.mainText
+
+        println(R.string.hello)
+        println(getString(R.string.hello))
+        println(resources.displayMetrics.heightPixels)
+        println(resources.displayMetrics.widthPixels)
+        println(resources.displayMetrics.densityDpi)
+        println(resources.displayMetrics.density)
     }
 }
